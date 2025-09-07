82eb0f0b33e54df689d43c9f2b7369080d475d76
riteesh-ram <bollavaramgolla.riteeshramchander.18cse@bmu.edu.in>
2025-09-07 22:18:04 +0100

    deleted the env

--- llm_testgen/mutants_validation/mutpy_env/bin/mut.py
+++
@@ -1,7 +0,0 @@
-#!/Users/riteeshram/Documents/Code Base/aster/llm_testgen/mutants_validation/mutpy_env/bin/python
-
-import sys
-from mutpy import commandline
-
-if __name__ == '__main__':
-    commandline.main(sys.argv)
